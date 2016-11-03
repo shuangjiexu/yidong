badcdf7ea91935f2bd9a7279dcd6c8b3c1482202
shuangjiexu <shuangjiexu@gmail.com>
2016-11-03 16:29:44 +0800

    Update MainFrame.py

--- MainFrame.py
+++ MainFrame.py
@@ -4,8 +4,6 @@ import wx
 import time
 import os
 
-import numpy
-
 ########################################################################
 # set the file filter
 wildcard1 = "Txt source (*.txt)|*.txt|"\
