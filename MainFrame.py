73dc6d6d6e6c9774d1d4047958221f31f58eca90
cbhust8025 <bocheng8025@gmail.com>
2016-11-04 16:58:08 +0800

    Update MainFrame.py

--- MainFrame.py
+++ MainFrame.py
@@ -3,10 +3,6 @@ import sys
 import wx
 import time
 import os
-import CaseOne
-import CaseTwo
-import CaseThree
-import BigDataAnalysis
 ########################################################################
 # set the file filter
 wildcard1 = "Txt source (*.txt)|*.txt|"\
@@ -25,239 +21,18 @@ class Demo(wx.Frame):
         self.Bind(wx.EVT_CLOSE, self.OnClose)
         self.Cflag = 0
 
+        #self.SetBackgroundColour("White")
         self.Panel1 = wx.Panel(self)
-        self.Panel1.SetBackgroundColour('Red')
+        #self.Panel1.SetBackgroundColour('Red')
 
         self.Panel2 = wx.Panel(self)
-        self.Panel2.SetBackgroundColour('Green')
+        #self.Panel2.SetBackgroundColour('Green')
 
         self.Panel3 = wx.Panel(self)
-        self.Panel3.SetBackgroundColour('Blue')
+        #self.Panel3.SetBackgroundColour('Blue')
 
         self.Panel4 = wx.Panel(self)
-        self.Panel4.SetBackgroundColour('Yellow')
-
-        #***************panel 1**********************************************
-        IndexSystemButton = wx.Button(self.Panel1, label=u'指标体系')
-        IndexSystemButton.Bind(wx.EVT_BUTTON, self.IndexSystem)
-
-        BigDataAnalysisButton = wx.Button(self.Panel1, label=u'大数据分析')
-        BigDataAnalysisButton.Bind(wx.EVT_BUTTON, self.BigDataAnalysis)
-
-        CaseShowButton = wx.Button(self.Panel1, label=u'案例展示')
-        CaseShowButton.Bind(wx.EVT_BUTTON, self.CaseShow)
-
-        CaseOneButton = wx.Button(self.Panel1, label=u'案例1:盲点检测展示')
-        CaseOneButton.Bind(wx.EVT_BUTTON, self.CaseOne)
-        CaseTwoButton = wx.Button(self.Panel1, label=u'案例2:用户密度预测')
-        CaseTwoButton.Bind(wx.EVT_BUTTON, self.CaseTwo)
-        CaseThreeButton = wx.Button(self.Panel1, label=u'案例3:异常信令事件分析')
-        CaseThreeButton.Bind(wx.EVT_BUTTON, self.CaseThree)
-        # ***************panel 1**********************************************
-
-        # ***************panel 2**********************************************
-        self.sampleList1 = {
-            u"接入类指标":0,
-            u"保持类指标":1,
-            u"信道类指标":2,
-            u"用户分布类指标":3,
-            u"用户感知类指标":4,
-            u"其他类指标":5
-        }
-        self.sampleList2 = [
-            [u"业务请求时延", u"加密时延", u"身份识别时延",u"TMSI重分配时延",u"语音寻呼时延"],
-            [u"剔除UI的无线掉线率"],
-            [u"阴影效应", u"多普勒效应", u"远近效应"],
-            [u"用户密度", u"用户密度对通信质量影响"],
-            [u"低接通用户数劣化指标", u"高掉线用户数劣化指标", u"高流量用户时延劣化指标",u"高流量用户接通劣化指标",
-             u"高时延用户数劣化指标"],
-            [u"上行业务质量", u"上行噪声影响", u"上行干扰对弱覆盖的影响",u"下行业务弱覆盖影响"]
-        ]
-        self.sample = wx.StaticText(self.Panel2, -1, u"指标类型:", style=wx.ALIGN_CENTER)
-        self.choices1 = wx.Choice(self.Panel2, -1,choices=self.sampleList1.keys(),style=wx.ALIGN_CENTER)
-
-        self.indexname = wx.StaticText(self.Panel2, -1, u"指标名称:", style=wx.ALIGN_CENTER)
-        self.choices2 = wx.Choice(self.Panel2, -1, choices=self.sampleList2[0], style=wx.ALIGN_CENTER)
-
-        self.choices1.Bind(wx.EVT_CHOICE, self.UpdateC2)
-        # ***************panel 2**********************************************
-
-        # ***************panel 3**********************************************
-        # ***************panel 3**********************************************
-
-        # ***************panel 4**********************************************
-        #状态栏
-        self.bar = wx.TextCtrl(self.Panel4, style=wx.TE_MULTILINE | wx.TE_RICH2)
-        # ***************panel 4**********************************************
-
-        # ***************panel 1 box set**********************************************
-        self.CSTbox = wx.BoxSizer(wx.VERTICAL)
-        self.CSTbox.Add(CaseOneButton, proportion=0, flag=wx.LEFT, border=0)
-        self.CSTbox.Add(CaseTwoButton, proportion=0, flag=wx.LEFT, border=0)
-        self.CSTbox.Add(CaseThreeButton, proportion=0, flag=wx.LEFT, border=0)
-
-        self.ISBbox = wx.BoxSizer(wx.VERTICAL)
-        self.ISBbox.Add(IndexSystemButton, proportion=0, flag=wx.LEFT, border=0)
-
-        self.BDAbox = wx.BoxSizer(wx.VERTICAL)
-        self.BDAbox.Add(BigDataAnalysisButton, proportion=0, flag=wx.LEFT, border=0)
-
-        self.CSbox = wx.BoxSizer(wx.VERTICAL)
-        self.CSbox.Add(CaseShowButton, proportion=0, flag=wx.LEFT, border=0)
-        self.CSbox.Add(self.CSTbox, proportion=0, flag=wx.LEFT, border=0)
-        self.CSbox.Hide(self.CSTbox)
-
-        self.hbox = wx.BoxSizer(wx.VERTICAL)
-        self.hbox.Add(self.ISBbox, proportion=0, flag=wx.LEFT | wx.TOP, border=20)
-        self.hbox.Add(self.BDAbox, proportion=0, flag=wx.LEFT | wx.TOP, border=20)
-        self.hbox.Add(self.CSbox, proportion=0, flag=wx.LEFT | wx.TOP, border=20)
-
-        self.vbox = wx.BoxSizer()
-        self.vbox.Add(self.hbox, proportion=0, flag=wx.ALL, border=10)
-
-        self.Panel1.SetSizer(self.vbox)
-        # ***************panel 1 box set**********************************************
-
-        # ***************panel 2 box set**********************************************
-        self.P2abox = wx.BoxSizer()
-        self.P2abox.Add(self.sample, proportion=0, flag=wx.LEFT | wx.TOP, border=20)
-        self.P2abox.Add(self.choices1, proportion=0, flag=wx.LEFT | wx.TOP, border=20)
-
-        self.P2abox.Add(self.indexname, proportion=0, flag=wx.LEFT | wx.TOP, border=20)
-        self.P2abox.Add(self.choices2, proportion=0, flag=wx.LEFT | wx.TOP, border=20)
-
-        self.Panel2.SetSizer(self.P2abox)
-        # ***************panel 2 box set**********************************************
-
-        # ***************panel 3 box set**********************************************
-        # ***************panel 3 box set**********************************************
-
-        # ***************panel 4 box set**********************************************
-        self.P4abox = wx.BoxSizer()
-        self.P4abox.Add(self.bar, proportion=1, flag=wx.EXPAND | wx.ALL, border=20)
-
-        self.Panel4.SetSizer(self.P4abox)
-        # ***************panel 4 box set**********************************************
-
-        # ***************window box set**********************************************
-        self.mbox = wx.BoxSizer(wx.VERTICAL)
-        self.mbox.Add(self.Panel2, proportion=2, flag=wx.ALL | wx.EXPAND, border=10)
-        self.mbox.Add(self.Panel3, proportion=20, flag=wx.ALL | wx.EXPAND, border=10)
-
-        self.tbox = wx.BoxSizer()
-        self.tbox.Add(self.Panel1, proportion=1, flag=wx.ALL | wx.EXPAND, border=10)
-        self.tbox.Add(self.mbox, proportion=5, flag=wx.ALL | wx.EXPAND, border=10)
-
-        self.wbox = wx.BoxSizer(wx.VERTICAL)
-        self.wbox.Add(self.tbox, proportion=10, flag=wx.ALL | wx.EXPAND, border=10)
-        self.wbox.Add(self.Panel4, proportion=1, flag=wx.ALL | wx.EXPAND, border=10)
-
-        self.SetSizer(self.wbox)
-        # ***************window box set**********************************************
-
-
-    def IndexSystem(self, evt):
-        pass
-
-    def BigDataAnalysis(self,evt):
-        BDA = BigDataAnalysis.BigDataAnalysis()
-        BDA.Show()
-
-    def CaseShow(self, evt):
-        if(self.Cflag == 0):
-            self.CSbox.Show(self.CSTbox)
-            self.vbox.Layout()
-            self.Cflag = 1
-        else:
-            self.CSbox.Hide(self.CSTbox)
-            self.vbox.Layout()
-            self.Cflag = 0
-
-
-    def CaseOne(self, evt):
-        case = CaseOne.CaseOne()
-        case.Show()
-
-    def CaseTwo(self, evt):
-        case = CaseTwo.CaseTwo()
-        case.Show()
-
-    def CaseThree(self, evt):
-        case = CaseThree.CaseThree()
-        case.Show()
-
-    def OnClose(self,evt):
-        evt.Skip()
-
-    def UpdateC2(self, evt):
-        self.choices2.Set(self.sampleList2[self.sampleList1[self.choices1.GetStringSelection()]])
-        self.P2abox.Layout()
-
-    def Show_Content(self, con):
-        self.bar.SetValue(con)
-        self.bar.SetInsertionPoint(0)
-        f = wx.Font(18, wx.ROMAN, wx.NORMAL, wx.NORMAL, False)
-        self.bar.SetStyle(0, self.bar.GetLastPosition(), wx.TextAttr("black", "white", f))
-
-
-
-
-class log(wx.Frame):
-    def __init__(self):
-        wx.Frame.__init__(self, None, -1,
-                          title=u"登陆",
-                          size=(555, 200),
-                          style=wx.DEFAULT_FRAME_STYLE)
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
-
-        account = wx.StaticText(self, -1, "Account:")
-        password = wx.StaticText(self, -1, "Password:")
-
-        self.Act = wx.TextCtrl(self, -1, "admin")
-        self.paw = wx.TextCtrl(self, -1, "12345", style=wx.TE_PASSWORD)
-
-        log_inbutton = wx.Button(self, label=u'登陆')
-        log_inbutton.Bind(wx.EVT_BUTTON, self.log_in)
-
-        exitbutton = wx.Button(self, label=u'退出')
-        exitbutton.Bind(wx.EVT_BUTTON, self.exit_out)
-
-        hbox = wx.BoxSizer(wx.VERTICAL)
-        hbox.Add(account, proportion=0, flag=wx.EXPAND, border=10)
-# coding:UTF-8
-import sys
-import wx
-import time
-import os
-########################################################################
-# set the file filter
-wildcard1 = "Txt source (*.txt)|*.txt|"\
-            "All files (*.*)|*.*|" \
-            "Python source (*.py; *.pyc)|*.py;*.pyc"
-wildcard2 = "Txt source (*.txt)|*.txt|"\
-    "Python source (*.py; *.pyc)|*.py;*.pyc|" \
-            "All files (*.*)|*.*"
-########################################################################
-class Demo(wx.Frame):
-    def __init__(self):
-        wx.Frame.__init__(self, None, -1,
-                          title="Demo platform",
-                          size=(1200, 1000),
-                          style=wx.DEFAULT_FRAME_STYLE)
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
-        self.Cflag = 0
-
-        self.Panel1 = wx.Panel(self)
-        self.Panel1.SetBackgroundColour('Red')
-
-        self.Panel2 = wx.Panel(self)
-        self.Panel2.SetBackgroundColour('Green')
-
-        self.Panel3 = wx.Panel(self)
-        self.Panel3.SetBackgroundColour('Blue')
-
-        self.Panel4 = wx.Panel(self)
-        self.Panel4.SetBackgroundColour('Yellow')
+        #self.Panel4.SetBackgroundColour('Yellow')
 
         #***************panel 1**********************************************
         IndexSystemButton = wx.Button(self.Panel1, label=u'指标体系')
@@ -326,6 +101,10 @@ class Demo(wx.Frame):
         self.ScreenDataButton = wx.Button(self.Panel2, label=u'筛选异常用户数据')
         self.ScreenDataButton.Bind(wx.EVT_BUTTON, self.ScreenData)
 
+        self.times1 = wx.StaticText(self.Panel2, -1, u"时段选择:", style=wx.ALIGN_CENTER)
+        self.sampleList6 = [u"时段1", u"时段2", u"时段3"]
+        self.choices6 = wx.Choice(self.Panel2, -1, choices=self.sampleList6, style=wx.ALIGN_CENTER)
+
         self.BlindspotMonitorButton = wx.Button(self.Panel2, label=u'盲点检测')
         self.BlindspotMonitorButton.Bind(wx.EVT_BUTTON, self.BlindspotMonitor)
 
@@ -342,6 +121,10 @@ class Demo(wx.Frame):
         self.regions = wx.StaticText(self.Panel2, -1, u"用户区域选择:", style=wx.ALIGN_CENTER)
         self.sampleList4 = [u"区域1", u"区域2", u"区域3"]
         self.choices4 = wx.Choice(self.Panel2, -1, choices=self.sampleList4, style=wx.ALIGN_CENTER)
+
+        self.times2 = wx.StaticText(self.Panel2, -1, u"时段选择:", style=wx.ALIGN_CENTER)
+        self.sampleList5 = [u"时段1", u"时段2", u"时段3"]
+        self.choices5 = wx.Choice(self.Panel2, -1, choices=self.sampleList5, style=wx.ALIGN_CENTER)
 
         self.UserPredictButton = wx.Button(self.Panel2, label=u'用户密度预测')
         self.UserPredictButton.Bind(wx.EVT_BUTTON, self.UserPredict)
@@ -421,6 +204,8 @@ class Demo(wx.Frame):
         self.Bhbox = wx.BoxSizer()
         self.Bhbox.Add(self.IDbox, proportion=0, flag=wx.ALL, border=10)
         self.Bhbox.Add(self.SDbox, proportion=0, flag=wx.ALL, border=10)
+        self.Bhbox.Add(self.times1, proportion=0, flag=wx.ALL, border=10)
+        self.Bhbox.Add(self.choices6, proportion=0, flag=wx.ALL, border=10)
         self.Bhbox.Add(self.BMbox, proportion=0, flag=wx.ALL, border=10)
         self.Bhbox.Add(self.RObox, proportion=0, flag=wx.ALL, border=10)
 
@@ -433,6 +218,8 @@ class Demo(wx.Frame):
         self.UPbox.Add(self.ScreenUserDataButton, proportion=0, flag=wx.ALL, border=10)
         self.UPbox.Add(self.regions, proportion=0, flag=wx.ALL, border=10)
         self.UPbox.Add(self.choices4, proportion=0, flag=wx.ALL, border=10)
+        self.UPbox.Add(self.times2, proportion=0, flag=wx.ALL, border=10)
+        self.UPbox.Add(self.choices5, proportion=0, flag=wx.ALL, border=10)
         self.UPbox.Add(self.UserPredictButton, proportion=0, flag=wx.ALL, border=10)
 
         # ***************panel 2 part 4 box set**********************************************
@@ -609,7 +396,13 @@ class Demo(wx.Frame):
         pass
 
     def ShowData(self, evt):
-        pass
+        dsSet = [20,50]
+        ds = DataShow(dsSet)
+        ds.Show()
+
+
+        print dsSet
+
 
     def Computing(self, evt):
         pass
@@ -634,7 +427,7 @@ class Demo(wx.Frame):
 
     def UpdateC2(self, evt):
         self.choices2.Set(self.sampleList2[self.sampleList1[self.choices1.GetStringSelection()]])
-        self.P2abox.Layout()
+        self.P2box.Layout()
 
     def Show_Content(self, con):
         self.bar.SetValue(con)
@@ -642,7 +435,78 @@ class Demo(wx.Frame):
         f = wx.Font(18, wx.ROMAN, wx.NORMAL, wx.NORMAL, False)
         self.bar.SetStyle(0, self.bar.GetLastPosition(), wx.TextAttr("black", "white", f))
 
+class DataShow(wx.Frame):
+    def __init__(self, ShowSet):
+        wx.Frame.__init__(self,None,-1,
+                          title=u"大数据分析-数据显示",
+                          size=(500,300),
+                          style=wx.DEFAULT_FRAME_STYLE
+        )
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+        self.Panel_ds = wx.Panel(self)
+        #self.Panel_ds.SetBackgroundColour('Red')
 
+        self.Panel_ds1 = wx.Panel(self)
+        #self.Panel_ds1.SetBackgroundColour('Green')
+
+        self.Panel_ds2 = wx.Panel(self)
+        #self.Panel_ds2.SetBackgroundColour('Blue')
+
+
+        self.title = wx.StaticText(self.Panel_ds1, -1, u"大数据平台数据显示设置:", style=wx.ALIGN_CENTER)
+
+        self.DataSource = wx.StaticText(self.Panel_ds, -1, u"数据源选择:", style=wx.ALIGN_CENTER)
+        self.sampleList = [u"数据源1", u"数据源2", u"数据源3"]
+        self.choices = wx.Choice(self.Panel_ds, -1, choices=self.sampleList, style=wx.ALIGN_CENTER)
+
+        self.datacols = wx.StaticText(self.Panel_ds, -1, u"数据行数:", style=wx.ALIGN_CENTER)
+        self.DataCols = wx.TextCtrl(self.Panel_ds, -1, r'1000')
+
+        self.SetButton = wx.Button(self.Panel_ds2, label=u'设置')
+        self.SetButton.Bind(wx.EVT_BUTTON, self.Set)
+        self.exitbutton = wx.Button(self.Panel_ds2, label=u'退出')
+        self.exitbutton.Bind(wx.EVT_BUTTON, self.exit_out)
+        #boxSizer set*********************************
+
+        self.P1box = wx.BoxSizer()
+        self.P1box.Add(self.title, proportion=0, flag=wx.ALL, border=10)
+        self.Panel_ds1.SetSizer(self.P1box)
+
+        self.P2box = wx.BoxSizer(wx.VERTICAL)
+        self.P2box.Add(self.SetButton, proportion=0, flag=wx.ALL, border=10)
+        self.P2box.Add(self.exitbutton, proportion=0, flag=wx.ALL, border=10)
+        self.Panel_ds2.SetSizer(self.P2box)
+
+        self.P221box = wx.BoxSizer()
+        self.P221box.Add(self.DataSource, proportion=0, flag=wx.ALL, border=10)
+        self.P221box.Add(self.choices, proportion=0, flag=wx.ALL, border=10)
+        self.P222box = wx.BoxSizer()
+        self.P222box.Add(self.datacols, proportion=0, flag=wx.ALL, border=10)
+        self.P222box.Add(self.DataCols, proportion=0, flag=wx.ALL, border=10)
+
+        self.P22box = wx.BoxSizer(wx.VERTICAL)
+        self.P22box.Add(self.P221box, proportion=0, flag=wx.ALL, border=10)
+        self.P22box.Add(self.P222box, proportion=0, flag=wx.ALL, border=10)
+        self.Panel_ds.SetSizer(self.P22box)
+
+        self.P3box = wx.BoxSizer()
+        self.P3box.Add(self.Panel_ds, proportion=0, flag=wx.ALL, border=10)
+        self.P3box.Add(self.Panel_ds2, proportion=0, flag=wx.ALL, border=10)
+
+        self.Pbox = wx.BoxSizer(wx.VERTICAL)
+        self.Pbox.Add(self.Panel_ds1, proportion=0, flag=wx.ALL, border=10)
+        self.Pbox.Add(self.P3box, proportion=0, flag=wx.ALL, border=10)
+
+        self.SetSizer(self.Pbox)
+        # boxSizer set*********************************
+    def Set(self, evt):
+        self.Show(False)
+
+    def exit_out(self, event):
+        self.Show(False)
+
+    def OnClose(self, evt):
+        evt.Skip()
 
 
 class log(wx.Frame):
